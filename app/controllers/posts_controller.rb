9af26db56211e38bb23bcc658beb01250992cf36
Jigesh Parekh <JParekh117@gmail.com>
2016-04-07 00:08:08 -0700

    create logic to label every 5th post as spam

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,12 @@ class PostsController < ApplicationController
   def index
 
   	@posts = Post.all
+
+  	 	@posts.each_with_index do |p, i|
+  		if i % 5 == 0 
+  			p.title += "SPAMSPAM"
+  		end
+  	end
   end
 
   def show
