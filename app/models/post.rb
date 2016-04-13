bcc9156051e4377f0cfc3893218d811a87940a8c
Jigesh Parekh <JParekh117@gmail.com>
2016-04-12 21:39:56 -0700

    Create a Delete function for Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-	has_many :comments
+	has_many :comments, dependent: :destroy
 	#allows post to have many comments related to it
 	#provides methods allow to retrieve comments that belong to post
 end
