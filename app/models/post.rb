c5aeadafba2ae158ae4cac8aee9bbfaae1dcd47b
Jigesh Parekh <JParekh117@gmail.com>
2016-04-17 20:02:40 -0700

    CP 35 Create Validation checkers for Post submission and edit

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,9 @@ class Post < ActiveRecord::Base
 	has_many :comments, dependent: :destroy
 	#allows post to have many comments related to it
 	#provides methods allow to retrieve comments that belong to post
+
+
+	validates :title, length: {minimum: 5}, presence: true
+	validates :body, length: { minimum: 20}, presence: true
+	validates :topic, presence: true
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -13,6 +13,13 @@ RSpec.describe Post, type: :model do
     let(:post) {topic.posts.create!(title:title, body:body)}
 
     it { is_expected.to belong_to(:topic)}
+
+    it { is_expected.to validate_presence_of(:title)}
+    it { is_expected.to validate_presence_of(:body)}
+    it { is_expected.to validate_presence_of(:topic)}
+
+    it { is_expected.to validate_length_of(:title).is_at_least(5)}
+    it { is_expected.to validate_length_of(:body).is_at_least(20)}
   #test post has attributes named title and body
   describe "attributes" do
   	it "has a title and body attributes" do
