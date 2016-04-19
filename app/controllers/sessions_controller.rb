bc5975f0b553196cc1fe2c8b2108d68427f76209
Jigesh Parekh <JParekh117@gmail.com>
2016-04-18 23:50:34 -0700

    CP38 Create Sign up page for new user as well as session manager

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+
+	def new
+
+	end
+
+	def create
+		user = User.find_by(email: params[:session][:email].downcase)
+
+		if user && user.authenticate(params[:session][:password])
+			create_session(user)
+			flash[:notice] = "Welcome, #{user.name}!"
+			redirect_to root_path
+		else
+			flash.now[:alert] = "invalid email/password combination"
+			render :new
+		end
+	end
+
+	def destroy
+		destroy_session(current_user)
+		flash[:notice] = "you've been signed out, come back soon!"
+		redirect_to root_path
+
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
    end
 
    resources :users, only: [:new, :create]
+
+   resources :sessions, only: [:new, :create, :destroy]
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
