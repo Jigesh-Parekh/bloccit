f4bc4cf683c9d5a70a9c24dbeeee1841035752b4
Jigesh Parekh <JParekh117@gmail.com>
2016-04-23 23:47:01 -0700

    Fix Moderator in topic and post controller to provide more secure logic for user functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -65,8 +65,8 @@ class PostsController < ApplicationController
      def authorize_user
      post = Post.find(params[:id])
  # #11
-     unless current_user == post.user || current_user.admin?
-       flash[:alert] = "You must be an admin to do that."
+     unless current_user == post.user || current_user.admin? || current_user.moderator>
+       flash[:alert] = "You must be an admin or mod to do that."
        redirect_to [post.topic, post]
      end
    end
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -4,7 +4,7 @@ class TopicsController < ApplicationController
 
 	before_action :authorize_user, except: [:index, :show]
 
-    before_action :authorize_mod, except: [:index, :show]
+
 
 	
 	def index
@@ -68,16 +68,14 @@ class TopicsController < ApplicationController
    end
 
    def authorize_user
-   		unless current_user.admin?
-   			flash[:alert] = "You must be an admin to do that."
-       		redirect_to topics_path
-       	end
+   	act = params["action"]
+   	if (act == "new" || act == "create" || act == "destroy") && !current_user.admin?
+      flash[:error] = "You must be an admin to do that."
+      redirect_to topics_path
+    elsif act == "update" && !(current_user.admin? || current_user.moderator?)
+      flash[:error] = "You must be an admin or moderator to do that."
+      redirect_to topics_path
+    end
    end
 
-   def authorize_mod
-   		unless current_user.moderator?
-   			flash[:alert] = "You must be an mod to do that."
-       		redirect_to topics_path
-       	end
-   end
 end
